1b506b10ffae2426880d10fc4e17c1e7f0e05758
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-12 16:32:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+var getFirstSelector = (selector) => {
+  return document.querySelector(selector)
+}
 
+var nestedTarget = () => {
+  return document.querySelector('#nested .target')
+}
+
+var increaseRankBy = (n) => {
+  const list = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+var deepestChild = () => {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node;
+}
